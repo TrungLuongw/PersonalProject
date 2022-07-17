d1d33e7545c2641ad3ac74a5eecc41bd93399c7c
Trung <luongquoctrung0120@gmail.com>
2022-07-17 21:36:38 +0700

    update

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
-const a = {
-  name: "trung",
-  age: 123,
-};
-for (let i in a) {
-  console.log(i, a[i]);
+function calculate(oldDate) {
+  let time = (Date.now() - new Date(oldDate)) / 1000;
+  if (time <= 60) {
+    return "few seconds ago";
+  } else if (time / 60 < 60) {
+    return `${Math.floor(time / 60)} minutes ago`;
+  } else if (time / 3600 < 60) {
+    return `${Math.floor(time / 3600)} giờ trước`;
+  } else {
+    return `${Math.floor(time / (3600 * 24))} ngay trước`;
+  }
 }
+
+const a = calculate("2022-07-14T04:52:35.790Z");
+console.log(a);
